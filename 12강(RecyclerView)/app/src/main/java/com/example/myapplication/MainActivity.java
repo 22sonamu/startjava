b8440319d97d136f4c61f73301212a917964975c
여정화 <22sonamu@naver.com>
2021-03-09 02:20:42 +0900

    12강

---
+++ 12강(RecyclerView)/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<MainData> arrayList;
+    private MainAdapter mainAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.rv);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList = new ArrayList<>();
+        mainAdapter = new MainAdapter(arrayList);
+        recyclerView.setAdapter(mainAdapter); //어댑터를 리사이클러뷰에 세팅
+
+
+        Button btn_add = (Button)findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainData mainData = new MainData(R.drawable.ic_launcher_background, "여정화", "리사이클러뷰");
+                arrayList.add(mainData); //이 데이터를 새로 추가
+                mainAdapter.notifyDataSetChanged(); //데이터 수정되었을때 해야함
+            }
+        });
+
+    }
+
+}
