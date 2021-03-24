9a243cfd48429d7e041bface8d3959a61017f7de
여정화 <22sonamu@naver.com>
2021-03-25 01:07:08 +0900

    네비게이션 뷰 추가

--- db_recyclerview/app/src/main/java/com/jungwha/db_recyclerview/MainActivity.java
+++ db_recyclerview/app/src/main/java/com/jungwha/db_recyclerview/MainActivity.java
@@ -1,12 +1,16 @@
 package com.jungwha.db_recyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
     private ArrayList<User> arrayList;
-
+    private DrawerLayout drawerLayout;
+    private View drawerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_add = (Button)findViewById(R.id.btn_add);
         db_recyclerview();
+        drawer();
+
 
 
         btn_add.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +73,72 @@ public class MainActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance(); //파이어베이스 데이터베이스를 가져와서 연동해라
         databaseReference = database.getReference("room"); //db 테이블 연결 //트리구조에서 room테이블을 가리킨다.
     }
+
+
+    public void drawer(){ //drawerlayout 관련 함수
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.drawer);
+
+        Button btn_open = (Button)findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener); //drawerlayout 리스너
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        Button btn_food = (Button)findViewById(R.id.btn_food); // drawer의 냉장고 탭
+        Button btn_inform = (Button)findViewById(R.id.btn_inform); //drawer의 회원정보 탭
+
+
+        btn_food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FoodActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_inform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FoodActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() { //drawerlayout 리스너 생성(특정 액션에 추가 기능)
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) { //drawer 열렸을때 상태값 설정란
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) { //drawer 닫혔을때 상태값 설정란
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
 }
 
 
